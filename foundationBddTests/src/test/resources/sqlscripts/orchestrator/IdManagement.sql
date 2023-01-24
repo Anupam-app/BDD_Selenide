8bbc7f36a933fe0eab73a41816528de30547b55b
Alexis Thiebaut <alexis.thiebaut@merckgroup.com>
2023-01-24 14:04:55 +0100

    fix language

--- foundationBddTests/src/test/resources/sqlscripts/orchestrator/IdManagement.sql
+++ foundationBddTests/src/test/resources/sqlscripts/orchestrator/IdManagement.sql
@@ -2,8 +2,8 @@ USE [IdManagement]
 GO
 DELETE FROM dbo.user_preferences;
 GO
-INSERT [dbo].[user_preferences] ( [preferences], [user_name], [default_view_id], [user_id], [user_lang_rfc]) VALUES ( N'{"preferredHomePage":"DeviceManagement"}', N'Bio4CAdmin', NULL, N'2', N'en-US')
+INSERT [dbo].[user_preferences] ( [preferences], [user_name], [default_view_id], [user_id], [user_lang_rfc]) VALUES ( N'{"preferredHomePage":"DeviceManagement"}', N'Bio4CAdmin', NULL, N'2', N'en-GB')
 GO
-INSERT [dbo].[user_preferences] ( [preferences], [user_name], [default_view_id], [user_id], [user_lang_rfc]) VALUES ( N'{"preferredHomePage":"DeviceManagement"}', N'testUserForI18N', NULL, N'18', N'en-US')
+INSERT [dbo].[user_preferences] ( [preferences], [user_name], [default_view_id], [user_id], [user_lang_rfc]) VALUES ( N'{"preferredHomePage":"DeviceManagement"}', N'testUserForI18N', NULL, N'18', N'en-GB')
 GO
 
