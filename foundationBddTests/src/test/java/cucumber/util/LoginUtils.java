13809300458393010f269920c0b74f11f613b124
Varun Mittal <varun.mittal@external.merckgroup.com>
2023-01-25 04:38:53 -0600

    updated password

--- foundationBddTests/src/test/java/cucumber/util/LoginUtils.java
+++ foundationBddTests/src/test/java/cucumber/util/LoginUtils.java
@@ -7,7 +7,7 @@ public class LoginUtils {
     public static String getPassword(String login) {
         switch (login.toUpperCase(Locale.ROOT)) {
             case "BIO4CADMIN":
-                return "MerckApp1@";
+                return "Merck@dmin";
             case "BIO4CSERVICE":
                 return "Merck$ervice";
             default:
