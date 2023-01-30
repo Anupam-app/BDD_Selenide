f0cb8ee3be7ac27fae2b3a0037ca76a3ab8d2a06
Anupam Patra <anupam.patra@external.merckgroup.com>
2023-01-30 10:36:10 +0530

    Updated the changes as per comment

--- foundationBddTests/src/test/java/pageobjects/pages/SettingPage.java
+++ foundationBddTests/src/test/java/pageobjects/pages/SettingPage.java
@@ -15,50 +15,50 @@ import org.openqa.selenium.By;
 import pageobjects.utility.SelenideHelper;
 
 public class SettingPage {
-    private SelenideElement settingsPageLinkText = $(By.id("ConfigurationManagement"));
-    private SelenideElement systemComponentText = $(By.xpath("//div[text()='System Components']"));
-    private SelenideElement inputCustomLabel = $(By.id("customerShortDescription"));
-    private SelenideElement applyButton = $(By.xpath("//button[contains(text(),'Apply')]"));
-    private SelenideElement customLabelText = $(By.xpath("//input[@id='customerShortDescription']"));
-    private SelenideElement settingsHeader = $(By.xpath("//div[text()='Settings' and @class='setting-header-title']"));
+	private SelenideElement settingsPageLinkText = $(By.id("ConfigurationManagement"));
+	private SelenideElement systemComponentText = $(By.xpath("//div[text()='System Components']"));
+	private SelenideElement inputCustomLabel = $(By.id("customerShortDescription"));
+	private SelenideElement applyButton = $(By.xpath("//button[contains(text(),'Apply')]"));
+	private SelenideElement customLabelText = $(By.xpath("//input[@id='customerShortDescription']"));
+	private SelenideElement settingsHeader = $(By.xpath("//div[text()='Settings' and @class='setting-header-title']"));
 
-    public void goToSettingsPage() {
-        SelenideHelper.commonWaiter(settingsPageLinkText, Condition.visible).click();
-    }
+	public void goToSettingsPage() {
+		SelenideHelper.commonWaiter(settingsPageLinkText, Condition.visible).click();
+	}
 
-        public void goToSystemComponents() {
-    	Selenide.sleep(3000);
-        systemComponentText.click();
-        
-    }
+	public void goToSystemComponents() {
+		Selenide.sleep(3000);
+		systemComponentText.click();
 
-    public void changeSettings(String customLabelName) throws AWTException {
-    	zoomOut();
-        $(By.id("customerShortDescription")).clear();
-        inputCustomLabel.setValue(customLabelName);
-        applyButton.click();
-        customLabelText.waitUntil(Condition.visible, 120001);
-        String s = customLabelText.getValue();
-        System.out.println(s);
+	}
 
-    }
+	public void changeSettings(String customLabelName) throws AWTException {
+		zoomOut();
+		$(By.id("customerShortDescription")).clear();
+		inputCustomLabel.setValue(customLabelName);
+		applyButton.click();
+		customLabelText.waitUntil(Condition.visible, 120001);
+		String s = customLabelText.getValue();
+		System.out.println(s);
 
-    public String getCustomLabelNameText() {
-        return customLabelText.getValue();
-    }
+	}
 
-    public void verifySettingHeader() {
-        settingsHeader.shouldBe(visible);
-    }
-    
-    private void zoomOut() throws AWTException {
-        Robot robot = new Robot();
-        for (int i = 0; i < 3; i++) {
-            robot.keyPress(KeyEvent.VK_CONTROL);
-            robot.keyPress(KeyEvent.VK_SUBTRACT);
-            robot.keyRelease(KeyEvent.VK_SUBTRACT);
-            robot.keyRelease(KeyEvent.VK_CONTROL);
-            Selenide.sleep(2000);
-            }
+	public String getCustomLabelNameText() {
+		return customLabelText.getValue();
+	}
+
+	public void verifySettingHeader() {
+		settingsHeader.shouldBe(visible);
+	}
+
+	private void zoomOut() throws AWTException {
+		Robot robot = new Robot();
+		for (int i = 0; i < 3; i++) {
+			robot.keyPress(KeyEvent.VK_CONTROL);
+			robot.keyPress(KeyEvent.VK_SUBTRACT);
+			robot.keyRelease(KeyEvent.VK_SUBTRACT);
+			robot.keyRelease(KeyEvent.VK_CONTROL);
+			Selenide.sleep(2000);
+		}
 	}
 }
