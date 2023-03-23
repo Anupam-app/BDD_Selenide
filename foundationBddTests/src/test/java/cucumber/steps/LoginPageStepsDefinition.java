b2ab201d7f869e1995f2a18fcb21190dc512579e
Srikanth Gajjela <srikanth.gajjela@external.merckgroup.com>
2023-03-23 17:47:50 +0530

    new step ADDED

--- foundationBddTests/src/test/java/cucumber/steps/LoginPageStepsDefinition.java
+++ foundationBddTests/src/test/java/cucumber/steps/LoginPageStepsDefinition.java
@@ -150,4 +150,12 @@ public class LoginPageStepsDefinition {
         loginPage.pushLogin();
     }
 
+    @When("I login with {string} as username and {string} as password")
+    public void iLoginWithValidCredential(String username, String password) {
+        loginPage.openLogin();
+        loginPage.setUser(username);
+        loginPage.setPassword(password);
+        loginPage.pushLogin();
+    }
+
 }
