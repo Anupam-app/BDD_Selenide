dd931cb4d622a3bcdcaf7b3a4ec547dba224a978
Varun Mittal <varun.mittal@external.merckgroup.com>
2023-04-04 22:59:33 -0500

    RecipeConsolePage.java edited online with Bitbucket

--- foundationBddTests/src/test/java/pageobjects/pages/RecipeConsolePage.java
+++ foundationBddTests/src/test/java/pageobjects/pages/RecipeConsolePage.java
@@ -158,11 +158,10 @@ public class RecipeConsolePage {
     }
 
     public void restartSystem() {
-        SelenideHelper.commonWaiter(restartButton, visible)
-                .click();
-        SelenideHelper.commonWaiter(yesButton, visible)
-                .click();
-        SelenideHelper.commonWaiter(holdButton, visible);
+        SelenideHelper.commonWaiter(restartButton, visible).click();
+        SelenideHelper.commonWaiter(reEstablishStateButton, enabled).click();
+        SelenideHelper.commonWaiter(confirmButton, visible).click();
+        holdButton.waitUntil(visible, 10000);
     }
 
     public void holdSystem() {
